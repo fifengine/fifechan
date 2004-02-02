1d4938ed257212fdf4f709ff751e67053692069b
Olof Næssén <olof.naessen@gmail.com>
2004-02-02 21:30:33 +0000

    Added new implemented headers such as mouseinput.hpp and keyinput.hpp.

--- include/guichan.hpp
+++ include/guichan.hpp
@@ -10,9 +10,11 @@
 //#include "guichan/gui.hpp"
 #include <guichan/image.hpp>
 // #include "guichan/imagemousetype.hpp"
-// #include "guichan/input.hpp"
-// #include "guichan/key.hpp"
+#include "guichan/input.hpp"
+#include "guichan/key.hpp"
+#include "guichan/keyinput.hpp"
 // #include "guichan/keylistener.hpp"
+#include "guichan/mouseinput.hpp"
 // #include "guichan/mouselistener.hpp"
 // #include "guichan/mousetype.hpp"
 #include <guichan/rectangle.hpp>
@@ -21,5 +23,6 @@
 
 #include <guichan/sdl/sdlgraphics.hpp>
 #include <guichan/sdl/sdlimage.hpp>
+#include <guichan/sdl/sdlinput.hpp>
 
 #endif // end GCN_GUICHAN_HPP
