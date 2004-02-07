e863dcb64371ee109fc6601a0ccaec31723fc706
Olof Næssén <olof.naessen@gmail.com>
2004-02-07 15:45:46 +0000

    Removed everyting regarding graphics/input drivers.

--- include/guichan.hpp
+++ include/guichan.hpp
@@ -10,19 +10,15 @@
 //#include "guichan/gui.hpp"
 #include <guichan/image.hpp>
 // #include "guichan/imagemousetype.hpp"
-#include "guichan/input.hpp"
-#include "guichan/key.hpp"
-#include "guichan/keyinput.hpp"
+#include <guichan/input.hpp>
+#include <guichan/key.hpp>
+#include <guichan/keyinput.hpp>
 // #include "guichan/keylistener.hpp"
-#include "guichan/mouseinput.hpp"
+#include <guichan/mouseinput.hpp>
 // #include "guichan/mouselistener.hpp"
 // #include "guichan/mousetype.hpp"
 #include <guichan/rectangle.hpp>
 #include <guichan/cliprectangle.hpp>
 //#include "guichan/widget.hpp"
-
-#include <guichan/sdl/sdlgraphics.hpp>
-#include <guichan/sdl/sdlimage.hpp>
-#include <guichan/sdl/sdlinput.hpp>
 
 #endif // end GCN_GUICHAN_HPP
