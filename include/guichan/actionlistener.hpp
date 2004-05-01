6be3a0fa43a5019e23ea09c03487f4a9e1409021
Olof Næssén <olof.naessen@gmail.com>
2004-05-01 21:39:12 +0000

    Added comments, renamed actionEvent to action

--- include/guichan/actionlistener.hpp
+++ include/guichan/actionlistener.hpp
@@ -6,20 +6,25 @@
 namespace gcn
 {
   /**
-   * 
+   * A ActionListener listens for action events from a widget.
+   *
+   * @see Widget::addActionListener
    */
   class ActionListener
   {
   public:
+
     /**
-     * 
+     * Destructor.
      */
-    virtual ~ActionListener();
+    virtual ~ActionListener() { }
     
     /**
-     * 
+     * This function is called upon an action recieved from a widget.
+     *
+     * @param eventId the identifier of the widget.
      */
-    virtual void actionEvent(const std::string& eventId) = 0;
+    virtual void action(const std::string& eventId) = 0;
 
   }; // end ActionListener
 
