ff056c7454cabdf3f2c0c39add0b864cd2b26ecd
Olof Næssén <olof.naessen@gmail.com>
2004-09-08 17:33:12 +0000

    Removed missing files, added new files

--- include/guichan.hpp
+++ include/guichan.hpp
@@ -61,13 +61,12 @@
 #include <guichan/graphics.hpp>
 #include <guichan/color.hpp>
 #include <guichan/exception.hpp>
-// #include "guichan/focushandler.hpp"
+#include <guichan/focushandler.hpp>
 #include <guichan/font.hpp>
 #include <guichan/gui.hpp>
 #include <guichan/image.hpp>
 #include <guichan/imagefont.hpp>
 #include <guichan/imageloader.hpp>
-// #include "guichan/imagemousetype.hpp"
 #include <guichan/input.hpp>
 #include <guichan/key.hpp>
 #include <guichan/keyinput.hpp>
@@ -75,11 +74,9 @@
 #include <guichan/listmodel.hpp>
 #include <guichan/mouseinput.hpp>
 #include <guichan/mouselistener.hpp>
-// #include "guichan/mousetype.hpp"
 #include <guichan/rectangle.hpp>
 #include <guichan/cliprectangle.hpp>
 
-#include <guichan/fontwidget.hpp>
 #include <guichan/widget.hpp>
 
 #include <guichan/widgets/button.hpp>
