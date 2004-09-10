1d5ea18ed03c91a8bcdf4fcbacec92167136911e
Olof Næssén <olof.naessen@gmail.com>
2004-09-10 19:10:13 +0000

    Added allegroinput.hpp

--- include/guichan/allegro/allegroinput.hpp
+++ include/guichan/allegro/allegroinput.hpp
@@ -1,34 +1,131 @@
+/*
+ *    _aaaa,  _aa.  sa,  aaa              _aaaa,_  ac  .aa.   .aa.  .aa,  _a, sa
+ *  .wWV!!!T  |Wm;  dQ[  $WF            _mWT!"?Y  ]QE  :Q#:   ]QW[  :WWk. ]Q[ dW
+ * .jWf       :WW: .dQ[  dQ[           .mW(       )WE  :Q#:  .mSQh. :mWQa.]W[ dQ
+ * |QW:       :Wm;  mQ[  dQ[           ]Qk        )Qmi_aQW:  <B:$Qc :WBWQ()W[ dQ
+ * |W#:  .ww  ;WW;  dQ[  dQ[  .......  ]Qk        )QB?YYW#:  jf ]Qp.:mE)Qm]Q[ )W
+ * +WQ;  :Wm  |Wm; .mQ[  dQ[ :qgggggga ]Qm.       ]WE  :Q# :=QasuQm;:Wk 3QQW[ )Y
+ *  ]Wmi.:Wm  +$Q; .mW(  dQ[  !"!!"!!^ dQk,  ._   ]WE  :Q# :3D"!!$Qc.Wk -$WQ[   
+ *   "?????? ` "?!=m?!   ??'            -??????!  -?!  -?? -?'   "?"-?"  "??' "?
+ *
+ * Copyright (c) 2004 darkbits                              Js_./
+ * Per Larsson a.k.a finalman                          _RqZ{a<^_aa
+ * Olof Naessén a.k.a jansem/yakslem                _asww7!uY`>  )\a//
+ *                                                 _Qhm`] _f "'c  1!5m
+ * Visit: http://guichan.darkbits.org             )Qk<P ` _: :+' .'  "{[
+ *                                               .)j(] .d_/ '-(  P .   S
+ * License: (BSD)                                <Td/Z <fP"5(\"??"\a.  .L
+ * Redistribution and use in source and          _dV>ws?a-?'      ._/L  #'
+ * binary forms, with or without                 )4d[#7r, .   '     )d`)[
+ * modification, are permitted provided         _Q-5'5W..j/?'   -?!\)cam'
+ * that the following conditions are met:       j<<WP+k/);.        _W=j f
+ * 1. Redistributions of source code must       .$%w\/]Q  . ."'  .  mj$
+ *    retain the above copyright notice,        ]E.pYY(Q]>.   a     J@\
+ *    this list of conditions and the           j(]1u<sE"L,. .   ./^ ]{a
+ *    following disclaimer.                     4'_uomm\.  )L);-4     (3=
+ * 2. Redistributions in binary form must        )_]X{Z('a_"a7'<a"a,  ]"[
+ *    reproduce the above copyright notice,       #}<]m7`Za??4,P-"'7. ).m
+ *    this list of conditions and the            ]d2e)Q(<Q(  ?94   b-  LQ/
+ *    following disclaimer in the                <B!</]C)d_, '(<' .f. =C+m
+ *    documentation and/or other materials      .Z!=J ]e []('-4f _ ) -.)m]'
+ *    provided with the distribution.          .w[5]' _[ /.)_-"+?   _/ <W"
+ * 3. Neither the name of darkbits nor the     :$we` _! + _/ .        j?
+ *    names of its contributors may be used     =3)= _f  (_yQmWW$#(    "
+ *    to endorse or promote products derived     -   W,  sQQQQmZQ#Wwa]..
+ *    from this software without specific        (js, \[QQW$QWW#?!V"".
+ *    prior written permission.                    ]y:.<\..          .
+ *                                                 -]n w/ '         [.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT       )/ )/           !
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY         <  (; sac    ,    '
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING,               ]^ .-  %
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF            c <   r
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR            aga<  <La
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE          5%  )P'-3L
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR        _bQf` y`..)a
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,          ,J?4P'.P"_(\?d'.,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES               _Pa,)!f/<[]/  ?"
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT      _2-..:. .r+_,.. .
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,     ?a.<%"'  " -'.a_ _,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION)                     ^
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef GCN_ALLEGROINPUT_HPP
 #define GCN_ALLEGROINPUT_HPP
 
+#include <allegro.h>
+
+#include <map>
 #include <queue>
 
 #include "guichan/input.hpp"
-#include "guichan/key.hpp"
 #include "guichan/keyinput.hpp"
 #include "guichan/mouseinput.hpp"
+#include "guichan/platform.hpp"
 
 namespace gcn
 {
   /**
-   * 
+   * This is an Allegro implementation of the Input class.
    */
-  class AllegroInput : public Input
+  class DECLSPEC AllegroInput : public Input
   {
   public:
 
-  protected:
-    unsigned char convertKeyCharacter(unsigned int unicode);
-    std::queue<KeyInput> mKeyInputQueue;
-    std::queue<MouseInput> mMouseInputQueue;
-    int mMouseX;
-    int mMouseY;
-    bool mMouseMotion;
+		AllegroInput();
+		
+    virtual ~AllegroInput(){ }
 
-    int mOldMouseButtons;
-    
+
+		// Inherited from Input
+
+    virtual bool isKeyQueueEmpty();
+
+    virtual KeyInput dequeueKeyInput();
+
+    virtual bool isMouseQueueEmpty();
+
+    virtual MouseInput dequeueMouseInput();
+
+    virtual void _pollInput();
+
+	protected:
+		/**
+		 * Handles the mouse input called by _pollInput
+		 */
+		virtual void pollMouseInput();
+
+		/**
+		 * Handles the key input called by _pollInput
+		 */
+		virtual void pollKeyInput();
+
+		/**
+		 * Converts scancode and unicode to Key obejct.
+		 */
+		virtual Key convertToKey(int scancode, int unicode);
+		
+		// This map holds the currently pressed Keys
+		// so we can send the correct key releases.
+		// it maps from scancode to key objects.
+		std::map<int, Key> mPressedKeys;
+
+		std::queue<KeyInput> mKeyQueue;
+    std::queue<MouseInput> mMouseQueue;
+		
+		bool mMouseButton1, mMouseButton2, mMouseButton3;
+		int mLastMouseX, mLastMouseY, mLastMouseZ;
   }; // end Input
   
 } // end gcn
 
-#endif // end GCN_ALLEGROINPUT_HPP
+#endif // end GCN_INPUT_HPP
+
+/*
+ * finalman - "A dyslectic walks in to a bra..."
+ * yakslem  - "...eh...ok..."
+ */
