5e71dfad8079a634fbf9fde6acd4fa5e9de43eb4
Olof Næssén <olof.naessen@gmail.com>
2004-09-10 19:10:58 +0000

    With allegroinput.hpp

--- include/guichan/allegro.hpp
+++ include/guichan/allegro.hpp
@@ -59,6 +59,6 @@
 
 #include <guichan/allegro/allegrographics.hpp>
 #include <guichan/allegro/allegroimageloader.hpp>
-//#include <guichan/allegro/allegroinput.hpp>
+#include <guichan/allegro/allegroinput.hpp>
 
 #endif // end GCN_ALLEGRO_HPP
