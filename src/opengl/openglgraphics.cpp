853c2c027a01743ab459a189c830e5ddf44cfbc5
Olof Næssén <olof.naessen@gmail.com>
2007-01-02 12:48:52 +0000

    Missplaced opengl lines have been removed.

--- src/opengl/openglgraphics.cpp
+++ src/opengl/openglgraphics.cpp
@@ -139,11 +139,6 @@ namespace gcn
 
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-
         glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
 
         pushClipArea(Rectangle(0, 0, mWidth, mHeight));
