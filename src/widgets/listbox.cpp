29eb696aaf7aa7a78cece7d25f27d0235e4559ed
Olof Næssén <olof.naessen@gmail.com>
2008-02-08 16:19:08 +0000

    A bug has been fixed where a list box didn't draw its elements correctly when having a negative y coordinate value due to an incorrect optimization.

--- src/widgets/listbox.cpp
+++ src/widgets/listbox.cpp
@@ -98,17 +98,38 @@ namespace gcn
         // that are not visible.
         const ClipRectangle currentClipArea = graphics->getCurrentClipArea();
         int fontHeight = getFont()->getHeight();
-        int i = currentClipArea.y / getFont()->getHeight();
-        int end = (currentClipArea.y + currentClipArea.height) / getFont()->getHeight();
+        
+		// Calculate the number of rows to draw by checking the clip area.
+		// The addition of two makes covers a partial visible row at the top
+		// and a partial visible row at the bottom.
+		int numberOfRows = currentClipArea.height / fontHeight + 2;
 
-        if (end > mListModel->getNumberOfElements())
+        if (numberOfRows > mListModel->getNumberOfElements())
         {
-            end = mListModel->getNumberOfElements();
+            numberOfRows = mListModel->getNumberOfElements();
         }
 
-        int y = 0;
+		// Calculate which row to start drawing. If the list box 
+		// has a negative y coordinate value we should check if
+		// we should drop rows in the begining of the list as
+		// they might not be visible. A negative y value is very
+		// common if the list box for instance resides in a scroll
+		// area and the user has scrolled the list box downwards.
+		int startRow;    	
+		if (getY() < 0)
+		{
+			startRow = -1 * (getY() / fontHeight);
+		}
+		else
+		{
+			startRow = 0;
+		}
 
-        for (i = 0; i < end; ++i)
+		int i;
+		// The y coordinate where we start to draw the text is
+		// simply the y coordinate multiplied with the font height.
+		int y = fontHeight * startRow;
+        for (i = startRow; i < startRow + numberOfRows; ++i)
         {
             if (i == mSelected)
             {
