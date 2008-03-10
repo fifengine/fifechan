df337fb783bc0f8ce1bab1bc672e301f88b9d364
Olof Næssén <olof.naessen@gmail.com>
2008-03-10 11:24:02 +0000

    Support for PSP SDK by Danny Angelo has been reverted as it broke the configure scripts when using SDL but not PSP.

--- buildpsp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# guichan.sh by Danny Angelo Carminati Grein (danny@bitcrafters.com.br)
-
-## Configure the build.
-LDFLAGS="-L$(psp-config --pspsdk-path)/lib -fno-exceptions -lc -lc -lpspuser" ./configure --host psp --prefix=$(psp-config --psp-prefix) --enable-pspirkeyb --enable-sdl --enable-sdlimage --enable-psp || { exit 1; }
-
-## Compile and install.
-make clean && make -j2 && make install && make clean || { exit 1; }
