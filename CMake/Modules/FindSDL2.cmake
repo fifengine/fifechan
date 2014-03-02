2acc0efd85d59ed348dae1a553efe0c57cde602d
helios2000 <helios2000@gmx.net>
2014-03-02 17:20:18 +0100

    Changed cmake modules and CMakeLists.txt.

--- CMake/Modules/FindSDL2.cmake
+++ CMake/Modules/FindSDL2.cmake
@@ -1,77 +1,91 @@
-# - Try to find SDL2
-# Once done this will define
+# - Find SDL2 library and headers
+# 
+# Find module for SDL 2.0 (http://www.libsdl.org/).
+# It defines the following variables:
+#  SDL2_INCLUDE_DIRS - The location of the headers, e.g., SDL.h.
+#  SDL2_LIBRARIES - The libraries to link against to use SDL2.
+#  SDL2_FOUND - If false, do not try to use SDL2.
+#  SDL2_VERSION_STRING - Human-readable string containing the version of SDL2.
 #
-#  SDL_FOUND - system has SDL2
-#  SDL_INCLUDE_DIR - the SDL2 include directory
-#  SDL_LIBRARY - Link these to use SDL2
+# This module responds to the the flag:
+#  SDL2_BUILDING_LIBRARY
+#    If this is defined, then no SDL2_main will be linked in because
+#    only applications need main().
+#    Otherwise, it is assumed you are building an application and this
+#    module will attempt to locate and set the the proper link flags
+#    as part of the returned SDL2_LIBRARIES variable.
 #
-#/***************************************************************************
-# *   Copyright (C) 2014 by the fifechan team                               *
-# *   http://fifechan.github.com/fifechan                                   *
-# *   This file is part of fifechan.                                        *
-# *                                                                         *
-# *   fifechan is free software; you can redistribute it and/or             *
-# *   modify it under the terms of the GNU Lesser General Public            *
-# *   License as published by the Free Software Foundation; either          *
-# *   version 2.1 of the License, or (at your option) any later version.    *
-# *                                                                         *
-# *   This library is distributed in the hope that it will be useful,       *
-# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
-# *   Lesser General Public License for more details.                       *
-# *                                                                         *
-# *   You should have received a copy of the GNU Lesser General Public      *
-# *   License along with this library; if not, write to the                 *
-# *   Free Software Foundation, Inc.,                                       *
-# *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA          *
-# ***************************************************************************/
+# Also defined, but not for general use are:
+#   SDL2_INCLUDE_DIR - The directory that contains SDL.h.
+#   SDL2_LIBRARY - The location of the SDL2 library.
+#   SDL2MAIN_LIBRARY - The location of the SDL2main library.
 #
 
+#=============================================================================
+# Copyright 2013 Benjamin Eikel
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of CMake, substitute the full
+#  License text for the above reference.)
 
-if (SDL_LIBRARY AND SDL_INCLUDE_DIR)
-  # in cache already
-  set(SDL_FOUND TRUE)
-else (SDL_LIBRARY AND SDL_INCLUDE_DIR)
-  find_path(SDL_INCLUDE_DIR
-    NAMES
-      SDL.h
-    PATHS
-      /usr/include
-      /usr/local/include
-      /opt/local/include
-      /usr/include/x86_64-linux-gnu
-    PATH_SUFFIXES
-      SDL2
-  )
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_SDL2 QUIET sdl2)
 
-  find_library(SDL_LIBRARY
-    NAMES
-      libSDL2
-    PATHS
-      /usr/lib
-      /usr/local/lib
-      /opt/local/lib
-      /usr/lib/x86_64-linux-gnu
-  )
-
-  set(SDL_INCLUDE_DIR
-    ${SDL_INCLUDE_DIR}
-  )
-  set(SDL_LIBRARY
-    ${SDL_LIBRARY}
+find_path(SDL2_INCLUDE_DIR
+  NAMES SDL.h
+  HINTS
+    ${PC_SDL2_INCLUDEDIR}
+    ${PC_SDL2_INCLUDE_DIRS}
+  PATH_SUFFIXES SDL2
 )
 
-  if (SDL_INCLUDE_DIR AND SDL_LIBRARY)
-     set(SDL_FOUND TRUE)
-  endif (SDL_INCLUDE_DIR AND SDL_LIBRARY)
+find_library(SDL2_LIBRARY
+  NAMES SDL2
+  HINTS
+    ${PC_SDL2_LIBDIR}
+    ${PC_SDL2_LIBRARY_DIRS}
+  PATH_SUFFIXES x64 x86
+)
 
-  if (SDL_FOUND)
-    message(STATUS "Found SDL2: ${SDL_LIBRARY}")
-  else (SDL_FOUND)
-    if (SDL_FIND_REQUIRED)
-      message(FATAL_ERROR "Could not find SDL2")
-    endif (SDL_FIND_REQUIRED)
-  endif (SDL_FOUND)
+if(NOT SDL2_BUILDING_LIBRARY)
+  find_library(SDL2MAIN_LIBRARY
+    NAMES SDL2main
+    HINTS
+      ${PC_SDL2_LIBDIR}
+      ${PC_SDL2_LIBRARY_DIRS}
+    PATH_SUFFIXES x64 x86
+  )
+endif()
 
-endif (SDL_LIBRARY AND SDL_INCLUDE_DIR)
+if(SDL2_INCLUDE_DIR AND EXISTS "${SDL2_INCLUDE_DIR}/SDL_version.h")
+  file(STRINGS "${SDL2_INCLUDE_DIR}/SDL_version.h" SDL2_VERSION_MAJOR_LINE REGEX "^#define[ \t]+SDL_MAJOR_VERSION[ \t]+[0-9]+$")
+  file(STRINGS "${SDL2_INCLUDE_DIR}/SDL_version.h" SDL2_VERSION_MINOR_LINE REGEX "^#define[ \t]+SDL_MINOR_VERSION[ \t]+[0-9]+$")
+  file(STRINGS "${SDL2_INCLUDE_DIR}/SDL_version.h" SDL2_VERSION_PATCH_LINE REGEX "^#define[ \t]+SDL_PATCHLEVEL[ \t]+[0-9]+$")
+  string(REGEX REPLACE "^#define[ \t]+SDL_MAJOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL2_VERSION_MAJOR "${SDL2_VERSION_MAJOR_LINE}")
+  string(REGEX REPLACE "^#define[ \t]+SDL_MINOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL2_VERSION_MINOR "${SDL2_VERSION_MINOR_LINE}")
+  string(REGEX REPLACE "^#define[ \t]+SDL_PATCHLEVEL[ \t]+([0-9]+)$" "\\1" SDL2_VERSION_PATCH "${SDL2_VERSION_PATCH_LINE}")
+  set(SDL2_VERSION_STRING ${SDL2_VERSION_MAJOR}.${SDL2_VERSION_MINOR}.${SDL2_VERSION_PATCH})
+  unset(SDL2_VERSION_MAJOR_LINE)
+  unset(SDL2_VERSION_MINOR_LINE)
+  unset(SDL2_VERSION_PATCH_LINE)
+  unset(SDL2_VERSION_MAJOR)
+  unset(SDL2_VERSION_MINOR)
+  unset(SDL2_VERSION_PATCH)
+endif()
 
+set(SDL2_INCLUDE_DIRS ${SDL2_INCLUDE_DIR})
+set(SDL2_LIBRARIES ${SDL2MAIN_LIBRARY} ${SDL2_LIBRARY})
+
+include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
+
+find_package_handle_standard_args(SDL2
+                                  REQUIRED_VARS SDL2_INCLUDE_DIR SDL2_LIBRARY
+                                  VERSION_VAR SDL2_VERSION_STRING)
+
+mark_as_advanced(SDL2_INCLUDE_DIR SDL2_LIBRARY)
--- CMake/Modules/FindSDL2_image.cmake
+++ CMake/Modules/FindSDL2_image.cmake
@@ -1,77 +1,73 @@
-# - Try to find SDL2 image
-# Once done this will define
+# - Find SDL2_image library and headers
+# 
+# Find module for SDL_image 2.0 (http://www.libsdl.org/projects/SDL_image/).
+# It defines the following variables:
+#  SDL2_IMAGE_INCLUDE_DIRS - The location of the headers, e.g., SDL_image.h.
+#  SDL2_IMAGE_LIBRARIES - The libraries to link against to use SDL2_image.
+#  SDL2_IMAGE_FOUND - If false, do not try to use SDL2_image.
+#  SDL2_IMAGE_VERSION_STRING
+#    Human-readable string containing the version of SDL2_image.
 #
-#  SDLIMAGE_FOUND - system has SDL2 image
-#  SDLIMAGE_INCLUDE_DIR - the SDL2 image include directory
-#  SDLIMAGE_LIBRARY - Link these to use SDL2 image
-#
-#/***************************************************************************
-# *   Copyright (C) 2014 by the fifechan team                               *
-# *   http://fifechan.github.com/fifechan                                   *
-# *   This file is part of fifechan.                                        *
-# *                                                                         *
-# *   fifechan is free software; you can redistribute it and/or             *
-# *   modify it under the terms of the GNU Lesser General Public            *
-# *   License as published by the Free Software Foundation; either          *
-# *   version 2.1 of the License, or (at your option) any later version.    *
-# *                                                                         *
-# *   This library is distributed in the hope that it will be useful,       *
-# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
-# *   Lesser General Public License for more details.                       *
-# *                                                                         *
-# *   You should have received a copy of the GNU Lesser General Public      *
-# *   License along with this library; if not, write to the                 *
-# *   Free Software Foundation, Inc.,                                       *
-# *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA          *
-# ***************************************************************************/
+# Also defined, but not for general use are:
+#   SDL2_IMAGE_INCLUDE_DIR - The directory that contains SDL_image.h.
+#   SDL2_IMAGE_LIBRARY - The location of the SDL2_image library.
 #
 
+#=============================================================================
+# Copyright 2013 Benjamin Eikel
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of CMake, substitute the full
+#  License text for the above reference.)
 
-if (SDLIMAGE_LIBRARY AND SDLIMAGE_INCLUDE_DIR)
-  # in cache already
-  set(SDLIMAGE_FOUND TRUE)
-else (SDLIMAGE_LIBRARY AND SDLIMAGE_INCLUDE_DIR)
-  find_path(SDLIMAGE_INCLUDE_DIR
-    NAMES
-      SDL_image.h
-    PATHS
-      /usr/include
-      /usr/local/include
-      /opt/local/include
-      /usr/include/x86_64-linux-gnu
-    PATH_SUFFIXES
-      SDL_image2
-  )
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_SDL2_IMAGE QUIET SDL2_image)
 
-  find_library(SDLIMAGE_LIBRARY
-    NAMES
-      libSDL2_image
-    PATHS
-      /usr/lib
-      /usr/local/lib
-      /opt/local/lib
-      /usr/lib/x86_64-linux-gnu
-  )
-
-  set(SDLIMAGE_INCLUDE_DIR
-    ${SDLIMAGE_INCLUDE_DIR}
-  )
-  set(SDLIMAGE_LIBRARY
-    ${SDLIMAGE_LIBRARY}
+find_path(SDL2_IMAGE_INCLUDE_DIR
+  NAMES SDL_image.h
+  HINTS
+    ${PC_SDL2_IMAGE_INCLUDEDIR}
+    ${PC_SDL2_IMAGE_INCLUDE_DIRS}
+  PATH_SUFFIXES SDL2
 )
 
-  if (SDLIMAGE_INCLUDE_DIR AND SDLIMAGE_LIBRARY)
-     set(SDLIMAGE_FOUND TRUE)
-  endif (SDLIMAGE_INCLUDE_DIR AND SDLIMAGE_LIBRARY)
+find_library(SDL2_IMAGE_LIBRARY
+  NAMES SDL2_image
+  HINTS
+    ${PC_SDL2_IMAGE_LIBDIR}
+    ${PC_SDL2_IMAGE_LIBRARY_DIRS}
+  PATH_SUFFIXES x64 x86
+)
 
-  if (SDLIMAGE_FOUND)
-    message(STATUS "Found SDL2 image: ${SDLIMAGE_LIBRARY}")
-  else (SDLIMAGE_FOUND)
-    if (SDLIMAGE_FIND_REQUIRED)
-      message(FATAL_ERROR "Could not find SDL2 image")
-    endif (SDLIMAGE_FIND_REQUIRED)
-  endif (SDLIMAGE_FOUND)
+if(SDL2_IMAGE_INCLUDE_DIR AND EXISTS "${SDL2_IMAGE_INCLUDE_DIR}/SDL_image.h")
+  file(STRINGS "${SDL2_IMAGE_INCLUDE_DIR}/SDL_image.h" SDL2_IMAGE_VERSION_MAJOR_LINE REGEX "^#define[ \t]+SDL_IMAGE_MAJOR_VERSION[ \t]+[0-9]+$")
+  file(STRINGS "${SDL2_IMAGE_INCLUDE_DIR}/SDL_image.h" SDL2_IMAGE_VERSION_MINOR_LINE REGEX "^#define[ \t]+SDL_IMAGE_MINOR_VERSION[ \t]+[0-9]+$")
+  file(STRINGS "${SDL2_IMAGE_INCLUDE_DIR}/SDL_image.h" SDL2_IMAGE_VERSION_PATCH_LINE REGEX "^#define[ \t]+SDL_IMAGE_PATCHLEVEL[ \t]+[0-9]+$")
+  string(REGEX REPLACE "^#define[ \t]+SDL_IMAGE_MAJOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL2_IMAGE_VERSION_MAJOR "${SDL2_IMAGE_VERSION_MAJOR_LINE}")
+  string(REGEX REPLACE "^#define[ \t]+SDL_IMAGE_MINOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL2_IMAGE_VERSION_MINOR "${SDL2_IMAGE_VERSION_MINOR_LINE}")
+  string(REGEX REPLACE "^#define[ \t]+SDL_IMAGE_PATCHLEVEL[ \t]+([0-9]+)$" "\\1" SDL2_IMAGE_VERSION_PATCH "${SDL2_IMAGE_VERSION_PATCH_LINE}")
+  set(SDL2_IMAGE_VERSION_STRING ${SDL2_IMAGE_VERSION_MAJOR}.${SDL2_IMAGE_VERSION_MINOR}.${SDL2_IMAGE_VERSION_PATCH})
+  unset(SDL2_IMAGE_VERSION_MAJOR_LINE)
+  unset(SDL2_IMAGE_VERSION_MINOR_LINE)
+  unset(SDL2_IMAGE_VERSION_PATCH_LINE)
+  unset(SDL2_IMAGE_VERSION_MAJOR)
+  unset(SDL2_IMAGE_VERSION_MINOR)
+  unset(SDL2_IMAGE_VERSION_PATCH)
+endif()
 
-endif (SDLIMAGE_LIBRARY AND SDLIMAGE_INCLUDE_DIR)
+set(SDL2_IMAGE_INCLUDE_DIRS ${SDL2_IMAGE_INCLUDE_DIR})
+set(SDL2_IMAGE_LIBRARIES ${SDL2_IMAGE_LIBRARY})
 
+include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
+
+find_package_handle_standard_args(SDL2_image
+                                  REQUIRED_VARS SDL2_IMAGE_INCLUDE_DIRS SDL2_IMAGE_LIBRARIES
+                                  VERSION_VAR SDL2_IMAGE_VERSION_STRING)
+
+mark_as_advanced(SDL2_IMAGE_INCLUDE_DIR SDL2_IMAGE_LIBRARY)
