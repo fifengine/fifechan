0bb1c2935e8057d0876ede943318d3df1ddcfec2
Thomas Kowaliczek <linuxdonald@posteo.de>
2016-10-15 18:48:07 +0200

    removed namespace contrib see: https://github.com/fifengine/fifechan/pull/22/commits/3f3b9787500769fc4f427259753cacd06b33fc06

--- include/fifechan/widgets/adjustingcontainer.hpp
+++ include/fifechan/widgets/adjustingcontainer.hpp
@@ -62,8 +62,8 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef FCN_CONTRIB_ADJUSTINGCONTAINER_HPP
-#define FCN_CONTRIB_ADJUSTINGCONTAINER_HPP
+#ifndef FCN_ADJUSTINGCONTAINER_HPP
+#define FCN_ADJUSTINGCONTAINER_HPP
 
 #include "fifechan/widgets/container.hpp"
 
@@ -71,188 +71,185 @@
 
 namespace fcn
 {
-    namespace contrib
-    {
-        /**
-         * Self-adjusting Container class. AdjustingContainers are an easy way to
-         * have Guichan position a group of widgets for you. It organizes elements
-         * in a table layout, with fixed columns and variable rows.  The user specifies
-         *
-         * @verbitam
-         * <ul>
-         *   <li>the number of columns</li>
-         *   <li>horizontal spacing between columns</li>
-         *   <li>vertical spacing between rows</li>
-         *   <li>padding around the sides of the container</li>
-         *   <li>each column's alignment</li>
-         * </ul>
-         * @endverbitam
-         *
-         * These properties give the user a lot of flexibility to make the
-         * widgets look just right.
-         * @code
-         * AdjustingContainer *adjust = new AdjustingContainer;
-         * adjust->setPadding(5, 5, 5, 5); //left, right, top, bottom
-         * adjust->setHorizontalSpacing(3);
-         * adjust->setVerticalSpacing(3);
-         * adjust->setColumns(3);
-         * adjust->setColumnAlignment(0, AdjustingContainer::LEFT);
-         * adjust->setColumnAlignment(1, AdjustingContainer::CENTER);
-         * adjust->setColumnAlignment(2, AdjustingContainer::RIGHT);
-         * top->add(adjust);
-         *
-         * for(int j = 0; j < 9; j++)
-         * {
-         *   fcn::Label *l;
-         *   int r = rand() % 3;
-         *   if(r == 0)
-         *     l = new fcn::Label("Short");
-         *   else if(r == 1)
-         *     l = new fcn::Label("A longer phrase");
-         *   else
-         *     l = new fcn::Label("Extravagent and wordy text");
-         *   adjust->add(l);
-         * @endcode
-         *
-         * Output:
-         * @verbitam
-         * <pre>
-         *+---------------------------------------------------------------------------+
-         *|                                                                           |
-         *| A longer phrase              Short             Extravagent and wordy text |
-         *|                                                                           |
-         *| Short             Extravagent and wordy text                        Short |
-         *|                                                                           |
-         *| Short                   A longer phrase                   A longer phrase |
-         *|                                                                           |
-         *+---------------------------------------------------------------------------+
-         * </pre>
-         * @endverbitam
-         * As you can see, each column is only as big as its largest element.
-         * The AdjustingContainer will resize itself and rearrange its contents
-         * based on whatever widgets it contains, allowing dynamic addition and
-         * removal while the program is running.  It also plays nicely with ScrollAreas,
-         * allowing you to show a fixed, maximum size while not limiting the actual
-         * container.
-         *
-         * For more help with using AdjustingContainers, try the Guichan forums
-         * (http://guichan.sourceforge.net/forum/) or email mrlachatte@gmail.com.
-         *
-         * @author Josh Matthews
-         */
-        class AdjustingContainer : public fcn::Container
-        {
-        public:
-            /**
-             * Constructor.
-             */
-            AdjustingContainer();
-        
-            /**
-             * Destructor.
-             */
-            virtual ~AdjustingContainer();
-        
-            /**
-             * Set the number of columns to divide the widgets into.
-             * The number of rows is derived automatically from the number
-             * of widgets based on the number of columns.  Default column
-             * alignment is left.
-             *
-             * @param numberOfColumns the number of columns.
-             */
-            virtual void setNumberOfColumns(unsigned int numberOfColumns);
-        
-            /**
-             * Set a specific column's alignment.
-             *
-             * @param column the column number, starting from 0.
-             * @param alignment the column's alignment. See enum with alignments.
-             */
-            virtual void setColumnAlignment(unsigned int column, unsigned int alignment);
-        
-            /**
-             * Set the padding for the sides of the container.
-             *
-             * @param paddingLeft left padding.
-             * @param paddingRight right padding.
-             * @param paddingTop top padding.
-             * @param paddingBottom bottom padding.
-             */
-            virtual void setPadding(unsigned int paddingLeft,
-                                    unsigned int paddingRight,
-                                    unsigned int paddingTop,
-                                    unsigned int paddingBottom);
-        
-            /**
-             * Set the spacing between rows.
-             *
-             * @param verticalSpacing spacing in pixels.
-             */
-            virtual void setVerticalSpacing(unsigned int verticalSpacing);
-        
-            /**
-             * Set the horizontal spacing between columns.
-             *
-             * @param horizontalSpacing spacing in pixels.
-             */
-            virtual void setHorizontalSpacing(unsigned int horizontalSpacing);
-        
-            /**
-             * Rearrange the widgets and resize the container.
-             */
-            virtual void adjustContent();
+	/**
+	 * Self-adjusting Container class. AdjustingContainers are an easy way to
+	 * have Guichan position a group of widgets for you. It organizes elements
+	 * in a table layout, with fixed columns and variable rows.  The user specifies
+	 *
+	 * @verbitam
+	 * <ul>
+	 *   <li>the number of columns</li>
+	 *   <li>horizontal spacing between columns</li>
+	 *   <li>vertical spacing between rows</li>
+	 *   <li>padding around the sides of the container</li>
+	 *   <li>each column's alignment</li>
+	 * </ul>
+	 * @endverbitam
+	 *
+	 * These properties give the user a lot of flexibility to make the
+	 * widgets look just right.
+	 * @code
+	 * AdjustingContainer *adjust = new AdjustingContainer;
+	 * adjust->setPadding(5, 5, 5, 5); //left, right, top, bottom
+	 * adjust->setHorizontalSpacing(3);
+	 * adjust->setVerticalSpacing(3);
+	 * adjust->setColumns(3);
+	 * adjust->setColumnAlignment(0, AdjustingContainer::LEFT);
+	 * adjust->setColumnAlignment(1, AdjustingContainer::CENTER);
+	 * adjust->setColumnAlignment(2, AdjustingContainer::RIGHT);
+	 * top->add(adjust);
+	 *
+	 * for(int j = 0; j < 9; j++)
+	 * {
+	 *   fcn::Label *l;
+	 *   int r = rand() % 3;
+	 *   if(r == 0)
+	 *     l = new fcn::Label("Short");
+	 *   else if(r == 1)
+	 *     l = new fcn::Label("A longer phrase");
+	 *   else
+	 *     l = new fcn::Label("Extravagent and wordy text");
+	 *   adjust->add(l);
+	 * @endcode
+	 *
+	 * Output:
+	 * @verbitam
+	 * <pre>
+	 *+---------------------------------------------------------------------------+
+	 *|                                                                           |
+	 *| A longer phrase              Short             Extravagent and wordy text |
+	 *|                                                                           |
+	 *| Short             Extravagent and wordy text                        Short |
+	 *|                                                                           |
+	 *| Short                   A longer phrase                   A longer phrase |
+	 *|                                                                           |
+	 *+---------------------------------------------------------------------------+
+	 * </pre>
+	 * @endverbitam
+	 * As you can see, each column is only as big as its largest element.
+	 * The AdjustingContainer will resize itself and rearrange its contents
+	 * based on whatever widgets it contains, allowing dynamic addition and
+	 * removal while the program is running.  It also plays nicely with ScrollAreas,
+	 * allowing you to show a fixed, maximum size while not limiting the actual
+	 * container.
+	 *
+	 * For more help with using AdjustingContainers, try the Guichan forums
+	 * (http://guichan.sourceforge.net/forum/) or email mrlachatte@gmail.com.
+	 *
+	 * @author Josh Matthews
+	 */
+	class AdjustingContainer : public fcn::Container
+	{
+	public:
+		/**
+		 * Constructor.
+		 */
+		AdjustingContainer();
+	
+		/**
+		 * Destructor.
+		 */
+		virtual ~AdjustingContainer();
+	
+		/**
+		 * Set the number of columns to divide the widgets into.
+		 * The number of rows is derived automatically from the number
+		 * of widgets based on the number of columns.  Default column
+		 * alignment is left.
+		 *
+		 * @param numberOfColumns the number of columns.
+		 */
+		virtual void setNumberOfColumns(unsigned int numberOfColumns);
+	
+		/**
+		 * Set a specific column's alignment.
+		 *
+		 * @param column the column number, starting from 0.
+		 * @param alignment the column's alignment. See enum with alignments.
+		 */
+		virtual void setColumnAlignment(unsigned int column, unsigned int alignment);
+	
+		/**
+		 * Set the padding for the sides of the container.
+		 *
+		 * @param paddingLeft left padding.
+		 * @param paddingRight right padding.
+		 * @param paddingTop top padding.
+		 * @param paddingBottom bottom padding.
+		 */
+		virtual void setPadding(unsigned int paddingLeft,
+								unsigned int paddingRight,
+								unsigned int paddingTop,
+								unsigned int paddingBottom);
+	
+		/**
+		 * Set the spacing between rows.
+		 *
+		 * @param verticalSpacing spacing in pixels.
+		 */
+		virtual void setVerticalSpacing(unsigned int verticalSpacing);
+	
+		/**
+		 * Set the horizontal spacing between columns.
+		 *
+		 * @param horizontalSpacing spacing in pixels.
+		 */
+		virtual void setHorizontalSpacing(unsigned int horizontalSpacing);
+	
+		/**
+		 * Rearrange the widgets and resize the container.
+		 */
+		virtual void adjustContent();
 
 
-            // Inherited from Container
+		// Inherited from Container
 
-            virtual void logic();
-        
-            virtual void add(Widget *widget);
+		virtual void logic();
+	
+		virtual void add(Widget *widget);
 
-            virtual void add(Widget *widget, int x, int y);
+		virtual void add(Widget *widget, int x, int y);
 
-            virtual void remove(Widget *widget);
+		virtual void remove(Widget *widget);
 
-            virtual void clear();
-               
-            /**
-             * Possible alignment values for each column.
-             *
-             * LEFT   - Align content to the left of the column.
-             * MIDDLE - Align content to the middle of the column.
-             * RIGHT  - Align content to the right of the column.
-             */
-            enum
-            {
-                LEFT = 0,
-                CENTER,
-                RIGHT
-            };
+		virtual void clear();
+		   
+		/**
+		 * Possible alignment values for each column.
+		 *
+		 * LEFT   - Align content to the left of the column.
+		 * MIDDLE - Align content to the middle of the column.
+		 * RIGHT  - Align content to the right of the column.
+		 */
+		enum
+		{
+			LEFT = 0,
+			CENTER,
+			RIGHT
+		};
 
-        protected:
-        
-            /**
-             * Adjust the size of the container to fit all the widgets.
-             */
-            virtual void adjustSize();
+	protected:
+	
+		/**
+		 * Adjust the size of the container to fit all the widgets.
+		 */
+		virtual void adjustSize();
 
-            std::vector<Widget*> mContainedWidgets;
-            std::vector<unsigned int> mColumnWidths;
-            std::vector<unsigned int> mColumnAlignment;
-            std::vector<unsigned int> mRowHeights;
-            unsigned int mWidth;
-            unsigned int mHeight;
-            unsigned int mNumberOfColumns;
-            unsigned int mNumberOfRows;
-            unsigned int mPaddingLeft;
-            unsigned int mPaddingRight;
-            unsigned int mPaddingTop;
-            unsigned int mPaddingBottom;
-            unsigned int mVerticalSpacing;
-            unsigned int mHorizontalSpacing;
-        };
-    }
+		std::vector<Widget*> mContainedWidgets;
+		std::vector<unsigned int> mColumnWidths;
+		std::vector<unsigned int> mColumnAlignment;
+		std::vector<unsigned int> mRowHeights;
+		unsigned int mWidth;
+		unsigned int mHeight;
+		unsigned int mNumberOfColumns;
+		unsigned int mNumberOfRows;
+		unsigned int mPaddingLeft;
+		unsigned int mPaddingRight;
+		unsigned int mPaddingTop;
+		unsigned int mPaddingBottom;
+		unsigned int mVerticalSpacing;
+		unsigned int mHorizontalSpacing;
+	};
 }
 
 #endif
--- src/widgets/adjustingcontainer.cpp
+++ src/widgets/adjustingcontainer.cpp
@@ -72,229 +72,226 @@
 
 namespace fcn
 {
-    namespace contrib
-    {
-        AdjustingContainer::AdjustingContainer()
-                : mWidth(0),
-                  mHeight(0),
-                  mNumberOfColumns(1),
-                  mNumberOfRows(1),
-                  mPaddingLeft(0),
-                  mPaddingRight(0),
-                  mPaddingTop(0),
-                  mPaddingBottom(0),
-                  mVerticalSpacing(0),
-                  mHorizontalSpacing(0)
-            
-            
-        {
-            mColumnWidths.push_back(0);
-            mRowHeights.push_back(0);
-        }
-    
-        AdjustingContainer::~AdjustingContainer()
-        {
+	AdjustingContainer::AdjustingContainer()
+			: mWidth(0),
+			  mHeight(0),
+			  mNumberOfColumns(1),
+			  mNumberOfRows(1),
+			  mPaddingLeft(0),
+			  mPaddingRight(0),
+			  mPaddingTop(0),
+			  mPaddingBottom(0),
+			  mVerticalSpacing(0),
+			  mHorizontalSpacing(0)
+		
+		
+	{
+		mColumnWidths.push_back(0);
+		mRowHeights.push_back(0);
+	}
 
-        }
+	AdjustingContainer::~AdjustingContainer()
+	{
 
-        void AdjustingContainer::setNumberOfColumns(unsigned int numberOfColumns)
-        {
-            mNumberOfColumns = numberOfColumns;
-        
-            if (mColumnAlignment.size() < numberOfColumns)
-            {
-                while (mColumnAlignment.size() < numberOfColumns)
-                {
-                    mColumnAlignment.push_back(LEFT);
-                }
-            }
-            else
-            {
-                while (mColumnAlignment.size() >  numberOfColumns)
-                {
-                    mColumnAlignment.pop_back();
-                }
-            }
-        }
+	}
 
-        void AdjustingContainer::setColumnAlignment(unsigned int column,
-                                                    unsigned int alignment)
-        {
-            if (column < mColumnAlignment.size())
-            {
-                mColumnAlignment[column] = alignment;
-            }
-        }
+	void AdjustingContainer::setNumberOfColumns(unsigned int numberOfColumns)
+	{
+		mNumberOfColumns = numberOfColumns;
+	
+		if (mColumnAlignment.size() < numberOfColumns)
+		{
+			while (mColumnAlignment.size() < numberOfColumns)
+			{
+				mColumnAlignment.push_back(LEFT);
+			}
+		}
+		else
+		{
+			while (mColumnAlignment.size() >  numberOfColumns)
+			{
+				mColumnAlignment.pop_back();
+			}
+		}
+	}
 
-        void AdjustingContainer::setPadding(unsigned int paddingLeft,
-                                            unsigned int paddingRight,
-                                            unsigned int paddingTop,
-                                            unsigned int paddingBottom)
-        {
-            mPaddingLeft = paddingLeft;
-            mPaddingRight = paddingRight;
-            mPaddingTop = paddingTop;
-            mPaddingBottom = paddingBottom;
-        }
+	void AdjustingContainer::setColumnAlignment(unsigned int column,
+												unsigned int alignment)
+	{
+		if (column < mColumnAlignment.size())
+		{
+			mColumnAlignment[column] = alignment;
+		}
+	}
 
-        void AdjustingContainer::setVerticalSpacing(unsigned int verticalSpacing)
-        {
-            mVerticalSpacing = verticalSpacing;
-        }
+	void AdjustingContainer::setPadding(unsigned int paddingLeft,
+										unsigned int paddingRight,
+										unsigned int paddingTop,
+										unsigned int paddingBottom)
+	{
+		mPaddingLeft = paddingLeft;
+		mPaddingRight = paddingRight;
+		mPaddingTop = paddingTop;
+		mPaddingBottom = paddingBottom;
+	}
 
-        void AdjustingContainer::setHorizontalSpacing(unsigned int horizontalSpacing)
-        {
-            mHorizontalSpacing = horizontalSpacing;
-        }
+	void AdjustingContainer::setVerticalSpacing(unsigned int verticalSpacing)
+	{
+		mVerticalSpacing = verticalSpacing;
+	}
 
-        void AdjustingContainer::logic()
-        {
-            Container::logic();
-            adjustContent();
-        }
-    
-        void AdjustingContainer::add(Widget *widget)
-        {
-            Container::add(widget);
-            mContainedWidgets.push_back(widget);
-        }
-    
-        void AdjustingContainer::add(Widget *widget, int x, int y)
-        {
-            add(widget);
-        }
-    
-        void AdjustingContainer::clear()
-        {
-            Container::clear();
-            mContainedWidgets.clear();
-        }
+	void AdjustingContainer::setHorizontalSpacing(unsigned int horizontalSpacing)
+	{
+		mHorizontalSpacing = horizontalSpacing;
+	}
 
-        void AdjustingContainer::remove(Widget *widget)
-        {
-            Container::remove(widget);
-            std::vector<fcn::Widget *>::iterator it;
-            for(it = mContainedWidgets.begin(); it != mContainedWidgets.end(); it++)
-            {
-                if(*it == widget)
-                {
-                    mContainedWidgets.erase(it);
-                    break;
-                }
-            }
-        }
+	void AdjustingContainer::logic()
+	{
+		Container::logic();
+		adjustContent();
+	}
 
-        void AdjustingContainer::adjustSize()
-        {
-            mNumberOfRows = mContainedWidgets.size()
-            / mNumberOfColumns + mContainedWidgets.size() % mNumberOfColumns;
+	void AdjustingContainer::add(Widget *widget)
+	{
+		Container::add(widget);
+		mContainedWidgets.push_back(widget);
+	}
 
-            mColumnWidths.clear();
+	void AdjustingContainer::add(Widget *widget, int x, int y)
+	{
+		add(widget);
+	}
 
-            unsigned int i;
-            for (i = 0; i < mNumberOfColumns; i++)
-            {
-                mColumnWidths.push_back(0);
-            }
-        
-            mRowHeights.clear();
+	void AdjustingContainer::clear()
+	{
+		Container::clear();
+		mContainedWidgets.clear();
+	}
 
-            for (i = 0; i < mNumberOfRows; i++)
-            {
-                mRowHeights.push_back(0);
-            }
+	void AdjustingContainer::remove(Widget *widget)
+	{
+		Container::remove(widget);
+		std::vector<fcn::Widget *>::iterator it;
+		for(it = mContainedWidgets.begin(); it != mContainedWidgets.end(); it++)
+		{
+			if(*it == widget)
+			{
+				mContainedWidgets.erase(it);
+				break;
+			}
+		}
+	}
 
-            for (i = 0; i < mNumberOfColumns; i++)
-            {
-                unsigned int j;
-                for (j = 0; j < mNumberOfRows && mNumberOfColumns * j + i < mContainedWidgets.size(); j++)
-                {
-                    if ((unsigned int)mContainedWidgets[mNumberOfColumns * j + i]->getWidth() > mColumnWidths[i])
-                    {
-                        mColumnWidths[i] = mContainedWidgets[mNumberOfColumns * j + i]->getWidth();
-                    }
-                    if ((unsigned int)mContainedWidgets[mNumberOfColumns * j + i]->getHeight() > mRowHeights[j])
-                    {
-                        mRowHeights[j] = mContainedWidgets[mNumberOfColumns * j + i]->getHeight();
-                    }
-                }
-            }
+	void AdjustingContainer::adjustSize()
+	{
+		mNumberOfRows = mContainedWidgets.size()
+		/ mNumberOfColumns + mContainedWidgets.size() % mNumberOfColumns;
 
-            mWidth = mPaddingLeft;
+		mColumnWidths.clear();
 
-            for (i = 0; i < mColumnWidths.size(); i++)
-            {
-                mWidth += mColumnWidths[i] + mHorizontalSpacing;
-            }
+		unsigned int i;
+		for (i = 0; i < mNumberOfColumns; i++)
+		{
+			mColumnWidths.push_back(0);
+		}
+	
+		mRowHeights.clear();
 
-            mWidth -= mHorizontalSpacing;
-            mWidth += mPaddingRight;
+		for (i = 0; i < mNumberOfRows; i++)
+		{
+			mRowHeights.push_back(0);
+		}
 
-            mHeight = mPaddingTop;
+		for (i = 0; i < mNumberOfColumns; i++)
+		{
+			unsigned int j;
+			for (j = 0; j < mNumberOfRows && mNumberOfColumns * j + i < mContainedWidgets.size(); j++)
+			{
+				if ((unsigned int)mContainedWidgets[mNumberOfColumns * j + i]->getWidth() > mColumnWidths[i])
+				{
+					mColumnWidths[i] = mContainedWidgets[mNumberOfColumns * j + i]->getWidth();
+				}
+				if ((unsigned int)mContainedWidgets[mNumberOfColumns * j + i]->getHeight() > mRowHeights[j])
+				{
+					mRowHeights[j] = mContainedWidgets[mNumberOfColumns * j + i]->getHeight();
+				}
+			}
+		}
 
-            for (i = 0; i < mRowHeights.size(); i++)
-            {
-                mHeight += mRowHeights[i] + mVerticalSpacing;
-            }
-        
-            mHeight -= mVerticalSpacing;
-            mHeight += mPaddingBottom;
+		mWidth = mPaddingLeft;
 
-            setHeight(mHeight);
-            setWidth(mWidth);
-        }
+		for (i = 0; i < mColumnWidths.size(); i++)
+		{
+			mWidth += mColumnWidths[i] + mHorizontalSpacing;
+		}
 
-        void AdjustingContainer::adjustContent()
-        {
-            adjustSize();
+		mWidth -= mHorizontalSpacing;
+		mWidth += mPaddingRight;
 
-            unsigned int columnCount = 0;
-            unsigned int rowCount = 0;
-            unsigned int y = mPaddingTop;
+		mHeight = mPaddingTop;
 
-            for (unsigned int i = 0; i < mContainedWidgets.size(); i++)
-            {
-                unsigned  basex;
-                if (columnCount % mNumberOfColumns)
-                {
-                    basex = mPaddingLeft;
-                    unsigned int j;
+		for (i = 0; i < mRowHeights.size(); i++)
+		{
+			mHeight += mRowHeights[i] + mVerticalSpacing;
+		}
+	
+		mHeight -= mVerticalSpacing;
+		mHeight += mPaddingBottom;
 
-                    for (j = 0; j < columnCount; j++)
-                    {
-                        basex += mColumnWidths[j] + mHorizontalSpacing;
-                    }
-                }
-                else
-                {
-                    basex = mPaddingLeft;
-                }
+		setHeight(mHeight);
+		setWidth(mWidth);
+	}
 
-                switch (mColumnAlignment[columnCount])
-                {
-                    case LEFT:
-                        mContainedWidgets[i]->setX(basex);
-                        break;
-                    case CENTER:
-                        mContainedWidgets[i]->setX(basex + (mColumnWidths[columnCount] - mContainedWidgets[i]->getWidth()) / 2);
-                        break;
-                    case RIGHT:
-                        mContainedWidgets[i]->setX(basex + mColumnWidths[columnCount] - mContainedWidgets[i]->getWidth());
-                        break;
-                    default:
-                        throw FCN_EXCEPTION("Unknown alignment.");
-                }
+	void AdjustingContainer::adjustContent()
+	{
+		adjustSize();
 
-                mContainedWidgets[i]->setY(y);
-                columnCount++;
+		unsigned int columnCount = 0;
+		unsigned int rowCount = 0;
+		unsigned int y = mPaddingTop;
 
-                if (columnCount == mNumberOfColumns)
-                {
-                    columnCount = 0;
-                    y += mRowHeights[rowCount] + mVerticalSpacing;
-                    rowCount++;
-                }
-            }
-        }
-    }
+		for (unsigned int i = 0; i < mContainedWidgets.size(); i++)
+		{
+			unsigned  basex;
+			if (columnCount % mNumberOfColumns)
+			{
+				basex = mPaddingLeft;
+				unsigned int j;
+
+				for (j = 0; j < columnCount; j++)
+				{
+					basex += mColumnWidths[j] + mHorizontalSpacing;
+				}
+			}
+			else
+			{
+				basex = mPaddingLeft;
+			}
+
+			switch (mColumnAlignment[columnCount])
+			{
+				case LEFT:
+					mContainedWidgets[i]->setX(basex);
+					break;
+				case CENTER:
+					mContainedWidgets[i]->setX(basex + (mColumnWidths[columnCount] - mContainedWidgets[i]->getWidth()) / 2);
+					break;
+				case RIGHT:
+					mContainedWidgets[i]->setX(basex + mColumnWidths[columnCount] - mContainedWidgets[i]->getWidth());
+					break;
+				default:
+					throw FCN_EXCEPTION("Unknown alignment.");
+			}
+
+			mContainedWidgets[i]->setY(y);
+			columnCount++;
+
+			if (columnCount == mNumberOfColumns)
+			{
+				columnCount = 0;
+				y += mRowHeights[rowCount] + mVerticalSpacing;
+				rowCount++;
+			}
+		}
+	}
 }
