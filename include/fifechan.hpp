843807778fdbb4b94b2c4a5218f7733fcbc1003a
Jens A. Koch <jakoch@web.de>
2024-07-21 01:14:55 +0200

    add asciiart into main header file

--- include/fifechan.hpp
+++ include/fifechan.hpp
@@ -2,6 +2,16 @@
 // SPDX-FileCopyrightText: 2004 - 2008 Olof Naessén and Per Larsson
 // SPDX-FileCopyrightText: 2013 - 2024 Fifengine contributors
 
+/*
+
+ ______   __     ______   ______     ______     __  __     ______     __   __
+/\  ___\ /\ \   /\  ___\ /\  ___\   /\  ___\   /\ \_\ \   /\  __ \   /\ "-.\ \
+\ \  __\ \ \ \  \ \  __\ \ \  __\   \ \ \____  \ \  __ \  \ \  __ \  \ \ \-.  \
+ \ \_\    \ \_\  \ \_\    \ \_____\  \ \_____\  \ \_\ \_\  \ \_\ \_\  \ \_\\"\_\
+  \/_/     \/_/   \/_/     \/_____/   \/_____/   \/_/\/_/   \/_/\/_/   \/_/ \/_/
+
+*/
+
 #ifndef FCN_FIFECHAN_HPP
 #define FCN_FIFECHAN_HPP
 
