16ae1f50eebc5d99a2095b9dc8f2b62e3f1a3865
Jens A. Koch <jakoch@web.de>
2024-07-21 16:29:07 +0200

    add doxygen comment for symbol visibility to platform.hpp

--- include/fifechan/platform.hpp
+++ include/fifechan/platform.hpp
@@ -5,12 +5,43 @@
 #ifndef FCN_PLATFORM_HPP
 #define FCN_PLATFORM_HPP
 
+/**
+ * @brief Symbol Visibility
+ *
+ * This header defines macros for symbol visibility in the library.
+ * The macros are used to define whether a symbol should be exported or imported.
+ * There are three cases to consider:
+ *    1. Building the core library
+ *    2. Building an extension to the core library
+ *    3. Using the core library or an extension in a client application
+ *
+ * @section core_library Building the core library
+ * When we are building the core library, we define `FIFECHAN_BUILD`.
+ * This makes sure the core library's symbols are exported (FIFEGUI_API as `__declspec(dllexport)`).
+ * Extension symbols would not be defined in this context.
+ *
+ * @section extension_library Building an extension to the core library
+ * If you are building an extension to the core library, you would define `FIFECHAN_EXTENSION_BUILD` in your project.
+ * This makes sure the core library's symbols are imported (FIFEGUI_API as `__declspec(dllimport)`),
+ * and the extension's symbols are exported (FIFEGUI_EXT_API as `__declspec(dllexport)`).
+ * You can do this by using set_target_properties in CMake:
+ * @code
+ * set_target_properties(${PROJECT_NAME}_yourextension PROPERTIES COMPILE_DEFINITIONS FIFECHAN_EXTENSION_BUILD)
+ *
+ * @section client_application Using the core library or an extension in a client application
+ * If you are using the core library or an extension in a client application,
+ * you would not define `FIFECHAN_BUILD` or `FIFECHAN_EXTENSION_BUILD`.
+ * For MinGW, you would define `FIFECHAN_DLL_IMPORT`.
+ * This makes sure the core library's symbols and the extension's symbols are imported
+ * (FIFEGUI_API and FIFEGUI_EXT_API as `__declspec(dllimport)`).
+ */
+
 #if defined(__MINGW32__) && defined(FIFECHAN_BUILD)
 #define FIFEGUI_API __declspec(dllexport)
 
 #elif defined(__MINGW32__) && defined(FIFECHAN_EXTENSION_BUILD)
-#define FIFEGUI_EXT_API __declspec(dllexport)
 #define FIFEGUI_API __declspec(dllimport)
+#define FIFEGUI_EXT_API __declspec(dllexport)
 
 #elif defined(__MINGW32__) && defined(FIFECHAN_DLL_IMPORT)
 #define FIFEGUI_API __declspec(dllimport)
