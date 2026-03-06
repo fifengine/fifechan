5e7915c553f365d79827e8703ef7fa3bbd263aea
Jens A. Koch <jakoch@web.de>
2026-03-06 20:28:02 +0100

    use TARGET_RUNTIME_DLLS to copy freetype.dll and libpng16.dll to example folders, too

--- CMake/InstallExample.cmake
+++ CMake/InstallExample.cmake
@@ -19,6 +19,15 @@ function(fifechan_install_example example_name)
       set_property(TARGET ${example_target} APPEND PROPERTY INSTALL_RPATH "\$ORIGIN/../../../../${CMAKE_INSTALL_LIBDIR}")
 
       if(WIN32)
+        add_custom_command(TARGET ${example_target} POST_BUILD
+          # Ensure runtime dependencies (for example freetype.dll and libpng16.dll)
+          # are available next to each built example executable on Windows.
+          COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            $<TARGET_RUNTIME_DLLS:${example_target}>
+            $<TARGET_FILE_DIR:${example_target}>
+          COMMAND_EXPAND_LISTS
+        )
+
         install(FILES $<TARGET_RUNTIME_DLLS:${example_target}>
           DESTINATION "${example_destination}"
         )
