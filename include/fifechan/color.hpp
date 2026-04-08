3fcb78cee558b0648dfa7d960b4d6f7d79822c4e
Jens A. Koch <jakoch@web.de>
2026-04-08 20:07:05 +0200

    fix swig error for "fcn:Color *' by adding explicit default destructor

--- include/fifechan/color.hpp
+++ include/fifechan/color.hpp
@@ -112,6 +112,11 @@ namespace fcn
          */
         Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255);
 
+        /**
+         * Destructor.
+         */
+        ~Color() = default;
+
         //
         // Arithmetic operators
         //
